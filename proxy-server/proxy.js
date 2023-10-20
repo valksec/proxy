d80e9b2e11704a9cef132e40c10df6b5159de4ea
ValkSec <148389872+valksec@users.noreply.github.com>
2023-10-19 20:50:14 -0400

    Update proxy.js

--- proxy-server/proxy.js
+++ proxy-server/proxy.js
@@ -1,29 +1,20 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
 
-app.get('/proxy', (req, res) => {
+app.get('/proxy', async (req, res) => {
   const url = req.query.url;
   if (!url) {
     return res.status(400).send('Missing URL parameter');
   }
-
-  // Check if the provided URL is valid
-  if (!isValidUrl(url)) {
-    return res.status(400).send('Invalid URL');
+  try {
+    const response = await axios.get(url);
+    res.send(response.data);
+  } catch (error) {
+    res.status(500).send('Error fetching the website');
   }
-
-  res.redirect(301, url);
 });
 
 app.listen(3000, () => {
   console.log('Proxy server is running on port 3000');
 });
-
-function isValidUrl(url) {
-  try {
-    new URL(url);
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
